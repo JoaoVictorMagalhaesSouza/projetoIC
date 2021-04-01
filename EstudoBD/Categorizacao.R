02aeae66d304ed177edd5ff7c963f831668a4b1e
JoaoVictorMagalhaesSouza <joao.souza5@ufv.br>
2021-04-01 20:50:44 -0300

    Foram feitos alguns ajustes de funcionamento e ajustes para melhorar a legibilidade.

--- EstudoBD/Categorizacao.R
+++ EstudoBD/Categorizacao.R
@@ -12,11 +12,11 @@ DI = '2020-01-01' #Data de inicio
 DF = Sys.Date() #Data de fim(hoje)
 benchmark = '^BVSP' #índice da bolsa
 
-ibov = GetIbovStocks() #Retorna as ações mais negociadas do Brasil, dados completos.
-ibov$tickersSA = paste(ibov$tickers,".SA",sep='') #Criar uma coluna e adicionar o .SA nos tickers
+tickersIbov = GetIbovStocks() #Retorna as ações mais negociadas do Brasil, dados completos.
+tickersIbov$tickersSA = paste(tickersIbov$tickers,".SA",sep='') #Criar uma coluna e adicionar o .SA nos tickers
 
 IBOVdatabase = BatchGetSymbols(
-  tickers = ibov$tickersSA, #Especificando as ações
+  tickers = tickersIbov$tickersSA, #Especificando as ações
   first.date = DI,
   last.date= DF,
   bench.ticker = benchmark,
@@ -26,13 +26,13 @@ IBOVdatabase = IBOVdatabase$df.tickers #Pegando o segundo elemento da lista reto
 IBOVdatabase = dlply(IBOVdatabase,.(ticker),function(x){rownames(x)=x$row;x$row=NULL;x}) #Lista com vários dataframes
 #Resumir o Banco de Dados
 
-databaseResumido = IBOVdatabase[[1]][,c(7,6)] #Extrair as colunas 7 e 6 do dataframe 1
-colnames(databaseResumido) = c("Data",paste(IBOVdatabase[[1]][1,8])) #Renomeando as colunas
+BancoDeDados_Acoes = IBOVdatabase[[1]][,c(7,6)] #Extrair as colunas 7 e 6 do dataframe 1
+colnames(BancoDeDados_Acoes) = c("Data",paste(IBOVdatabase[[1]][1,8])) #Renomeando as colunas
 
 for(i in 2:length(IBOVdatabase)){
-  itera_databaseResumido = IBOVdatabase[[i]][,c(7,6)] 
-  colnames(itera_databaseResumido) = c("Data",paste(IBOVdatabase[[i]][1,8])) #Renomeando as colunas
-  databaseResumido = merge(databaseResumido,itera_databaseResumido, by = "Data") #Juntando os dataframes usando a Data como coluna chave para fazer os joins.
+  itera_BancoDeDados_Acoes = IBOVdatabase[[i]][,c(7,6)] 
+  colnames(itera_BancoDeDados_Acoes) = c("Data",paste(IBOVdatabase[[i]][1,8])) #Renomeando as colunas
+  BancoDeDados_Acoes = merge(BancoDeDados_Acoes,itera_BancoDeDados_Acoes, by = "Data") #Juntando os dataframes usando a Data como coluna chave para fazer os joins.
 }
 
 #df_info = get_info_companies()
@@ -40,14 +40,14 @@ for(i in 2:length(IBOVdatabase)){
 
 ##Banco de Dados mais apurado com descrição e os tickers. 
 df = gdfpd.get.info.companies()
-df_reduzido = subset(df,select = c(1,11,12,13,14))
-df_reduzido = df_reduzido[!duplicated(df_reduzido), ] #Tirar os duplicados
-df_reduzido = subset(df_reduzido,df_reduzido[5] != "") #Tirar as empresas que não tem um pregão
-names(df_reduzido) = c("Nome da Empresa","Setor","Subsetor","Segmento","Tickers")
+DataFrame_Empresas = subset(df,select = c(1,11,12,13,14))
+DataFrame_Empresas = DataFrame_Empresas[!duplicated(DataFrame_Empresas), ] #Tirar os duplicados
+DataFrame_Empresas = subset(DataFrame_Empresas,DataFrame_Empresas[5] != "") #Tirar as empresas que não tem um pregão
+names(DataFrame_Empresas) = c("Nome da Empresa","Setor","Subsetor","Segmento","Tickers")
 
 #TESTANDO FILTRO
 
-#tick = df_reduzido$Tickers
+#tick = DataFrame_Empresas$Tickers
 #print(tick[1])
 #v = strsplit(tick[1],";")
 #print(length(v[[1]]))
@@ -69,19 +69,19 @@ names(df_reduzido) = c("Nome da Empresa","Setor","Subsetor","Segmento","Tickers"
 
 #IMPLEMENTANDO FILTRO
 #Montar um dataframe com os dados de um setor específico.
-
-subsetor = "Construção e Engenharia"
-energia = subset(df_reduzido,df_reduzido[3]==subsetor)
-energia_lista = energia$Tickers
-nlinhas = nrow(energia)
-numcol = ncol(databaseResumido)
-final <- data.frame(Data=c(databaseResumido[1]))
+ 
+subsetor = "Construção Civil"     #"Construção e Engenharia"
+Acoes_Filtradas = subset(DataFrame_Empresas,DataFrame_Empresas[3]==subsetor)
+Acoes_Filtradas_lista = Acoes_Filtradas$Tickers
+nlinhas = nrow(Acoes_Filtradas)
+numcol = ncol(BancoDeDados_Acoes)
+DataFrame_Final <- data.frame(Data=c(BancoDeDados_Acoes[1]))
 for(i in 1:nlinhas){
-  tickers = strsplit(energia_lista[i],";")
+  tickers = strsplit(Acoes_Filtradas_lista[i],";")
   for (j in 1:length(tickers[[1]])){
     for (k in 2:numcol){
-    if (paste(tickers[[1]][j],"SA",sep=".") == names(databaseResumido[k])){
-     final[paste(tickers[[1]][j],"SA",sep=".")] = subset(databaseResumido,select = c(k))
+    if (paste(tickers[[1]][j],"SA",sep=".") == names(BancoDeDados_Acoes[k])){
+     DataFrame_Final[paste(tickers[[1]][j],"SA",sep=".")] = subset(BancoDeDados_Acoes,select = c(k))
       print("Achei")
     
       }
@@ -90,13 +90,16 @@ for(i in 1:nlinhas){
   
 }
 
-df = melt(final,id.vars = "Data", variable.name = "Ações")
+df = melt(DataFrame_Final,id.vars = "Data", variable.name = "Ações")
 
 ggplot(df,aes(Data,value)) + geom_line(aes(colour = Ações))
 
 
+#Criar uma lista dos setores.
+
+
 #Uma empresa com mais de um pregão a pesquisa não funciona.
-pesquisa = subset(df_reduzido,df_reduzido[5]=="B3SA3")
+#pesquisa = subset(DataFrame_Empresas,DataFrame_Empresas[5]=="B3SA3")
 #df_reduce = subset(df_info,df_info[11]=="ATIVO")
 #df_reduce = subset(df_reduce, df_reduce[43]=="BOLSA")
 #df_reduce = subset(df_reduce, df_reduce[15]=="Categoria A")
