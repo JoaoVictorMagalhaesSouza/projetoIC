7be15abfa277177dd7edf1f0c8389c34e069c21c
JoaoVictorMagalhaesSouza <joao.souza5@ufv.br>
2021-05-27 09:24:30 -0300

    Algumas mudanças para otimização de funcionamento do código.

--- EstudoBD/Code.R
+++ EstudoBD/Code.R
@@ -53,6 +53,7 @@ for(i in 2:length(IBOVdatabase)){
   colnames(itera_BancoDeDados_Acoes) = c("Data",paste(IBOVdatabase[[i]][1,7])) #Renomeando as colunas
   BancoDeDados_Acoes = merge(BancoDeDados_Acoes,itera_BancoDeDados_Acoes, by = "Data") #Juntando os dataframes usando a Data como coluna chave para fazer os joins.
 }
+rm(itera_BancoDeDados_Acoes)
 
 #df_info = get_info_companies()
 #names(df_info)
@@ -83,102 +84,86 @@ df_emp$Subsetor<- str_to_upper(df_emp$Subsetor)
 df_emp$Segmento<- str_to_upper(df_emp$Segmento)
 df_emp$Tickers <- str_to_upper(df_emp$Tickers )
 
-bimestre = 2
-mes1 <- paste("0",as.character((bimestre*2)-1),sep = "")
-mes2 <- paste("0",as.character(bimestre*2),sep="")
-j = 1
-dataIni = as.integer(strsplit(DI,"-")[[1]][1])+1
-dataF = as.integer(strsplit(as.character(DF),"-")[[1]][1])
-qtdeGraf <- dataF - dataIni #Quantidade de gráficos a serem gerados.
 
-dataAtual <- dataIni
-ploter <- list()
-boxers <- list()
-for (i in 1:qtdeGraf){
-  final <- 1
-  contador <- 0
-  inicial <- 0
-  for (j in 1:nrow(BancoDeDados_Acoes)){
-    #Ano
-    if (strsplit(as.character(BancoDeDados_Acoes$Data[j]),"-")[[1]][1] == dataAtual){
-      #Bimestre que eu quero
-      if(strsplit(as.character(BancoDeDados_Acoes$Data[j]),"-")[[1]][2]==mes1 || strsplit(as.character(BancoDeDados_Acoes$Data[j]),"-")[[1]][2]==mes2){
-        final <- j
-        print(BancoDeDados_Acoes$Data[j])
-        contador <- contador + 1
-      }
-      inicial <- final-contador+1
-    }
-  }
-  print(final)
-  print(contador)
-  print(inicial)
-  
-  #Plotar aqui
-  ploter[[i]] <- BancoDeDados_Acoes[inicial:final,] %>%        
-    select(Data,ABEV3.SA,BBAS3.SA) %>% 
-    melt(id.var = "Data") %>% 
-    ggplot(aes(Data,value))+geom_line(aes(colour = variable))
-  dataAtual <- dataAtual+1
-  boxers[[i]] = melt(BancoDeDados_Acoes[inicial:final,],id.vars = "Data", measure.vars = c("B3SA3.SA"))
-  
-  
-}
-ggplotly(ploter[[5]])
-ggplot(boxers[[1]],aes(x=Data,y=value))+geom_boxplot()
-
-
-
-# for (i in linha:nrow(BancoDeDados_Acoes)){
-#      if(strsplit(as.character(BancoDeDados_Acoes$Data[i]),"-")[[1]][2]=="05" || strsplit(as.character(BancoDeDados_Acoes$Data[i]),"-")[[1]][2]=="06"){
-#        print(BancoDeDados_Acoes$Data[i])
-#        plot[j] <- BancoDeDados_Acoes %>%        
-#         select(Data,BBDC3.SA) %>% 
-#         melt(id.var = "Data") %>% 
-#         ggplot(aes(Data,value))+geom_line(aes(colour = variable))
-#         j <- j+1
-#      }
-# }
-# ggplotly(plot)
 setores = subset(df_emp, select = c(2))
 setores = setores[!duplicated(setores),]
+#Criar uma funcao para verificar se determinado ativo esta no BD Yahoo.
+verificar_coluna <- function(data, coluna){
+  retorno <- coluna
+  retorno %in% names(data)
+}
+#aux <- "B3SA3.SA"
+#verificar_coluna(BancoDeDados_Acoes,aux)
 
- #IMPLEMENTANDO FILTRO
- #Montar um dataframe com os dados de um setor específico.
- print(setores[[1]][9])
- setor = setores[[1]][9]    #Saúde
+ #Escolher um setor específico
+ setor = setores[[1]][10]    #Saúde
+ #Pegar todas as empresas desse setor:
  Acoes_Filtradas = subset(df_emp,df_emp[2]==setor)
+ #Pegar todos os tickers dessas empresas desse setor:
  Acoes_Filtradas_lista = Acoes_Filtradas$Tickers
+ #Obter o numero de linhas do BD das acoes do setor especificado
  nlinhas <- nrow(Acoes_Filtradas)
+ #Conferir coluna a coluna 
   numcol = ncol(BancoDeDados_Acoes)
+  #Pegando a coluna "Data" do BancoDeDados_Acoes
  DataFrame_Final <- data.frame(Data=c(BancoDeDados_Acoes[1]))
+ teste <- data.frame(Data=c(BancoDeDados_Acoes[1]))
+ #Vamos conferir quais  os tickers desse BD Auxiliar(no setor escolhido) estao no BD do Yahoo.
  for(i in 1:nlinhas){
   tickers = strsplit(Acoes_Filtradas_lista[i],";")
    for (j in 1:length(tickers[[1]])){
-     for (k in 2:numcol){
-     if (paste(tickers[[1]][j],"SA",sep=".") == names(BancoDeDados_Acoes[k])){
-      DataFrame_Final[paste(tickers[[1]][j],"SA",sep=".")] = subset(BancoDeDados_Acoes,select = c(k))
-       print("Achei")
+     aux <- paste(tickers[[1]][j],"SA",sep=".")
+      if (verificar_coluna(BancoDeDados_Acoes,aux)){
+        teste[aux] =  select(BancoDeDados_Acoes,aux)
+      }
      
-       }
-     }
    }
    
  }
-# 
-# df = melt(DataFrame_Final,id.vars = "Data", variable.name = "Ações")
-# 
-# ggplot(df,aes(Data,value)) + geom_line(aes(colour = Ações))
-# ##Pipe: pega o que vem antes e joga na função posterior.
-# library(plotly)
-#  plot <- BancoDeDados_Acoes %>% 
-#   select(Data,ABEV3.SA,AZUL4.SA,B3SA3.SA) %>% 
-#   melt(id.var = "Data") %>% 
-#   ggplot(aes(Data,value))+geom_line(aes(colour = variable))
-# ggplotly(plot)
-#  
-# 
-# 
-# df.m <- melt(BancoDeDados_Acoes, id.var = "Data")
-# ggplot(df.m,aes(variable,value))+geom_boxplot()
-# 
+ 
+ 
+ 
+ bimestre = 2
+ mes1 <- paste("0",as.character((bimestre*2)-1),sep = "")
+ mes2 <- paste("0",as.character(bimestre*2),sep="")
+ j = 1
+ dataIni = as.integer(strsplit(DI,"-")[[1]][1])+1
+ dataF = as.integer(strsplit(as.character(DF),"-")[[1]][1])
+ qtdeGraf <- dataF - dataIni #Quantidade de gráficos a serem gerados.
+ 
+ dataAtual <- dataIni
+ ploter <- list()
+ boxers <- list()
+ for (i in 1:qtdeGraf){
+   final <- 1
+   contador <- 0
+   inicial <- 0
+   for (j in 1:nrow(BancoDeDados_Acoes)){
+     #Ano
+     if (strsplit(as.character(BancoDeDados_Acoes$Data[j]),"-")[[1]][1] == dataAtual){
+       #Bimestre que eu quero
+       if(strsplit(as.character(BancoDeDados_Acoes$Data[j]),"-")[[1]][2]==mes1 || strsplit(as.character(BancoDeDados_Acoes$Data[j]),"-")[[1]][2]==mes2){
+         final <- j
+         print(BancoDeDados_Acoes$Data[j])
+         contador <- contador + 1
+       }
+       inicial <- final-contador+1
+     }
+   }
+   print(final)
+   print(contador)
+   print(inicial)
+   
+   #Plotar aqui
+   ploter[[i]] <- BancoDeDados_Acoes[inicial:final,] %>%        
+     select(Data,ABEV3.SA,BBAS3.SA) %>% 
+     melt(id.var = "Data") %>% 
+     ggplot(aes(Data,value))+geom_line(aes(colour = variable))
+   dataAtual <- dataAtual+1
+   boxers[[i]] = melt(BancoDeDados_Acoes[inicial:final,],id.vars = "Data", measure.vars = c("B3SA3.SA"))
+   
+   
+ }
+ ggplotly(ploter[[5]])
+ ggplot(boxers[[1]],aes(x=Data,y=value))+geom_boxplot()
+ 
