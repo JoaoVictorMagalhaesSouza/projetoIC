3e78901614f91824d570eef8d177bc3373939e6d
JoaoVictorMagalhaesSouza <joao.souza5@ufv.br>
2021-11-18 11:46:29 -0300

    Dygraph Catboost

--- catboost.R
+++ catboost.R
@@ -5,6 +5,13 @@ library("BatchGetSymbols")
 library("dplyr")
 library("data.table")
 library("caret")
+library("rpart.plot")
+library("GGally")
+library("ggplot2")
+library("dygraphs")
+library("xts")
+library("lubridate")
+library("plotly")
 createDataSet <- function(acao){
   DI = '2015-01-01' #Data de inicio
   DF = Sys.Date() #Data de fim(hoje)
@@ -51,9 +58,11 @@ createMetrics <- function (DataSet){
   
 }
 
-df = createDataSet("CMIG4.SA")
+df = createDataSet("CIEL3.SA")
 datas <- df$ref.date
-df <- df[,-c("ret.closing.prices","ticker","ref.date")]
+df <- df[,-c("ret.closing.prices","ticker","ref.date","price.low","price.high")]
+mat_corr <- ggcorr(df)
+mat_corr
 #write.csv(df,"b3unormalized.csv", row.names = FALSE)
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
@@ -75,15 +84,15 @@ prediction <- catboost.predict(model, real_pool,verbose = TRUE,thread_count=4)
 print(prediction)
 prediction <- data.frame(prediction)
 
-p = ggplot(col=) +
-
-  geom_line(data = prediction, aes (x= datas,y=prediction), color = "blue") +
-  geom_line(data = maxmindf, aes(x=datas,y=price.close),color="red",) +
-  xlab('Dates') +
-  ylab('percent.change')+
-
-
-print(p)
+# p = ggplot() +
+# 
+#   geom_line(data = prediction, aes (x= datas,y=prediction), color = "blue") +
+#   geom_line(data = maxmindf, aes(x=datas,y=price.close),color="red",) +
+#   xlab('Dates') +
+#   ylab('percent.change')+
+# 
+# 
+# print(p)
 maxmindf$data = datas
 prediction$data = datas
 maxmindf <- maxmindf[,c("data","price.close")]
@@ -92,3 +101,14 @@ names(prediction)[1] <- c("Preço previsto")
 prescription = merge(prediction, maxmindf, by="data")
 prescriptionMelted <- reshape2::melt(prescription, id.var='data')
 ggplot(prescriptionMelted, aes(x=data, y=value, col=variable)) + geom_line()
+
+bd = data.table()
+bd$Data = datas
+bd$`Preço Real` = maxmindf$`Preço Real`
+bd$`Preço Previsto` = prediction$`Preço previsto`
+df <- bd %>% 
+  select(Data,`Preço Real`, `Preço Previsto`)   
+don <- xts(order.by = bd$Data,x = bd[,-1])
+dygraph(don,main="Preço Real x Preço Previsto",xlab = "Data",ylab = "Preço de Fechamento(R$)") %>%
+  dyOptions(stackedGraph = TRUE) %>%    
+  dyRangeSelector(height = 20)
